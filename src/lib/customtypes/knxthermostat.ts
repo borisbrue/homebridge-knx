f38154fd37084bceeff4d30641c28caf14ac0aaf
borisbrue <boris@zuckersalzundpfeffer.de>
2026-05-19 09:54:46 +0200

    ts tooling added

--- src/lib/customtypes/knxthermostat.ts
+++ src/lib/customtypes/knxthermostat.ts
@@ -1,24 +1,24 @@
 'use strict';
 
-import { inherits } from 'util';
-import { Formats, Perms } from 'homebridge';
 import type { API } from 'homebridge';
+import { Formats, Perms } from 'homebridge';
+
+const UUID = '00001025-0000-1000-8000-0026BB765292';
 
 export function registerKnxThermostat(homebridgeAPI: API): void {
-  const Characteristic = homebridgeAPI.hap.Characteristic;
-  const props = {
-    format: Formats.BOOL,
-    perms: [Perms.PAIRED_READ, Perms.PAIRED_WRITE, Perms.NOTIFY],
+  const hap = homebridgeAPI.hap;
+
+  const KNXThermAtHome = class extends hap.Characteristic {
+    static readonly UUID = UUID;
+
+    constructor() {
+      super('At Home', UUID, {
+        format: Formats.BOOL,
+        perms: [Perms.PAIRED_READ, Perms.PAIRED_WRITE, Perms.NOTIFY],
+      });
+      this.value = this.getDefaultValue();
+    }
   };
 
-  function KNXThermAtHome(this: unknown) {
-    (Characteristic as unknown as (this: unknown, name: string, uuid: string, props: object) => void)
-      .call(this, 'At Home', '00001025-0000-1000-8000-0026BB765292', props);
-    (this as { value: unknown; getDefaultValue: () => unknown }).value =
-      (this as { getDefaultValue: () => unknown }).getDefaultValue();
-  }
-
-  inherits(KNXThermAtHome, Characteristic);
-  (KNXThermAtHome as unknown as Record<string, unknown>)['UUID'] = '00001025-0000-1000-8000-0026BB765292';
-  (Characteristic as unknown as Record<string, unknown>)['KNXThermAtHome'] = KNXThermAtHome;
+  (hap.Characteristic as unknown as Record<string, unknown>)['KNXThermAtHome'] = KNXThermAtHome;
 }
